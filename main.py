d7155fa742351060abf1cc2596071148d17ee998
Jim Hult <jimhult@hotmail.se>
2023-10-27 23:23:20 +0200

    Dynamic game generation of lines

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import pygame
 
+ZONES = 3
+
 
 def main():
     # Init Pygame
     pygame.init()
 
-    mainScreen = pygame.display.set_mode((900, 700))  # Window size
+    mainScreen = pygame.display.set_mode((900, 900))
     pygame.display.set_caption("Tic Tac Toe")
-    drawBoardLines(mainScreen)
+    draw_tic_tac_toe_board(mainScreen, ZONES)
 
     # Main game loop
     running = True
@@ -26,25 +28,28 @@ def main():
     pygame.quit()
 
 
-def drawBoardLines(screen):
+# Function to draw a Tic-Tac-Toe board of a given size
+def draw_tic_tac_toe_board(screen, size):
     whiteColor = (255, 255, 255)
-    lineWidth = 5
-    # Line 1 Vertical
-    startPos = (300, 100)
-    endPos = (300, 600)
-    pygame.draw.line(screen, whiteColor, startPos, endPos, lineWidth)
-    # Line 2 Vertical
-    startPos = (550, 100)
-    endPos = (550, 600)
-    pygame.draw.line(screen, whiteColor, startPos, endPos, lineWidth)
-    # Line 1 Horizontal
-    startPos = (150, 250)
-    endPos = (700, 250)
-    pygame.draw.line(screen, whiteColor, startPos, endPos, lineWidth)
-    # Line 2 Horizontal
-    startPos = (150, 450)
-    endPos = (700, 450)
-    pygame.draw.line(screen, whiteColor, startPos, endPos, lineWidth)
+    blackColor = (0, 0, 0)
+    board_size = min(screen.get_width(), screen.get_height())
+    cell_size = board_size // size
+
+    # TODO: in this func, use the screen size calc to get the dimensions of the boxes
+    # This is then going to be used to identify in what boxes the user klicks in
+
+    # Clear the screen
+    screen.fill(blackColor)
+
+    # Draw horizontal lines
+    for row in range(1, size):
+        y = row * cell_size
+        pygame.draw.line(screen, whiteColor, (0, y), (board_size - 0, y), 2)
+
+    # Draw vertical lines
+    for col in range(1, size):
+        x = col * cell_size
+        pygame.draw.line(screen, whiteColor, (x, 0), (x, board_size - 0), 2)
 
 
 if __name__ == "__main__":
