089fb009371a0d01ad02b08a369b71bad51c1980
Jimsan <jimhult@hotmail.se>
2023-10-31 22:02:43 +0100

    types everywhere & Cell class & Mark enum

--- board.py
+++ board.py
@@ -1,10 +1,25 @@
 import pygame
+from enum import Enum
+
+class Mark(Enum):
+    undefined = 0
+    O = 1
+    X = 2
+
+class Cell():
+    def __init__(self, topLeft: tuple[int, int], botRight: tuple[int, int], mark: Mark = Mark.undefined) -> None:
+        self.topLeft: tuple[int, int] = topLeft
+        self.botRight: tuple[int, int] = botRight
+        self.mark: Mark = mark
+    
+    def isMarked(self) -> bool:
+        return self.mark != Mark.undefined
 
 class Board():
-    def __init__(self, screen_size, num_zones) -> None:
-        self.num_zones = num_zones
-        self.board_size = screen_size
-        self.cell_size = min(self.board_size) // num_zones
+    def __init__(self, screen_size: tuple[int, int], num_cells: int) -> None:
+        self.num_cells: int = num_cells
+        self.board_size: tuple[int, int] = screen_size
+        self.cell_size: int = min(self.board_size) // num_cells
 
         # This creates list of all cells
         # Format example: 
@@ -12,13 +27,20 @@ class Board():
         # [((0, 300), (300, 600)), ((300, 300), (600, 600)), ((600, 300), (900, 600))], 
         # [((0, 600), (300, 900)), ((300, 600), (600, 900)), ((600, 600), (900, 900))]]
         # Starts horizontally with the x row
-        self.cells = [[((i * self.cell_size, j * self.cell_size), ((i+1) * self.cell_size, (j+1) * self.cell_size)) 
-                       for i in range(num_zones)] for j in range(num_zones)]
+        temp_cells = [[(i * self.cell_size, j * self.cell_size, (i+1) * self.cell_size, (j+1) * self.cell_size) 
+                       for i in range(num_cells)] for j in range(num_cells)]
+        
+        self.cells: list[Cell]
+        for horizontallCells in temp_cells:
+            for cell in horizontallCells:
+                self.cells.append(Cell(cell[0:1], cell[2:3])) # WIP
+        
         print(self.cells[0][0])
 
 
+
     # Function to draw a Tic-Tac-Toe board of a given size
-    def draw_board(self, screen):
+    def draw_board(self, screen: pygame.Surface):
         whiteColor = (255, 255, 255)
         blackColor = (0, 0, 0)
 
@@ -28,14 +50,22 @@ class Board():
         screen.fill(blackColor)
 
         # Draw horizontal lines
-        for row in range(1, self.num_zones):
+        for row in range(1, self.num_cells):
             y = row * self.cell_size
             pygame.draw.line(screen, whiteColor, (0, y), (min(self.board_size) - 0, y), 2)
 
         # Draw vertical lines
-        for col in range(1, self.num_zones):
+        for col in range(1, self.num_cells):
             x = col * self.cell_size
             pygame.draw.line(screen, whiteColor, (x, 0), (x, min(self.board_size) - 0), 2)
     
+        # # Test to see self.cells
+        # for horizontallCells in self.cells:
+        #     for cell in horizontallCells:
+        #         pygame.draw.rect(screen, (255, 0, 0), cell, 5)
+    
+    def markCell(self, mark: Mark, coordinate: tuple[int, int, int, int]):
+
+        pass
 
 
