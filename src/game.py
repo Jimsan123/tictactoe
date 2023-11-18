0bc3bacb7360286f39acbbcf1e20d41d105bb56d
Jim Hult <jimhult@hotmail.se>
2023-11-18 01:18:03 +0100

    fixed wrong indentation

--- src/game.py
+++ src/game.py
@@ -2,7 +2,6 @@ import pygame
 from board import Board, Mark
 
 
-
 class Game():
     def __init__(self, mainScreen: pygame.Surface, board: Board, in_row_to_win: int) -> None:
         self.board = board
@@ -51,37 +50,33 @@ class Game():
         # Check diagonals
 
         # Function to check a single diagonal
-            def check_diagonal(start_row, start_col, row_increment, col_increment):
-                winCount = 0
-                row, col = start_row, start_col
-                while 0 <= row < self.board.num_cells and 0 <= col < self.board.num_cells:
-                    if self.board.cells[row][col].mark == playerMark:
-                        winCount += 1
-                        if winCount == self.in_row_to_win:
-                            return True
-                    else:
-                        winCount = 0  # reset count if sequence is broken
-
-                    row += row_increment
-                    col += col_increment
-
-                return False
-
-            # Check all diagonals from top left to bottom right
-            for start_row in range(self.board.num_cells - self.in_row_to_win + 1):
-                for start_col in range(self.board.num_cells - self.in_row_to_win + 1):
-                    if check_diagonal(start_row, start_col, 1, 1):
-                        print("Won on a diagonal from top left to bottom right")
+        def check_diagonal(start_row, start_col, row_increment, col_increment):
+            winCount = 0
+            row, col = start_row, start_col
+            while 0 <= row < self.board.num_cells and 0 <= col < self.board.num_cells:
+                if self.board.cells[row][col].mark == playerMark:
+                    winCount += 1
+                    if winCount == self.in_row_to_win:
                         return True
+                else:
+                    winCount = 0  # reset count if sequence is broken
+                row += row_increment
+                col += col_increment
+            return False
 
-            # Check all diagonals from top right to bottom left
-            for start_row in range(self.board.num_cells - self.in_row_to_win + 1):
-                for start_col in range(self.in_row_to_win - 1, self.board.num_cells):
-                    if check_diagonal(start_row, start_col, 1, -1):
-                        print("Won on a diagonal from top right to bottom left")
-                        return True
+        # Check all diagonals from top left to bottom right
+        for start_row in range(self.board.num_cells - self.in_row_to_win + 1):
+            for start_col in range(self.board.num_cells - self.in_row_to_win + 1):
+                if check_diagonal(start_row, start_col, 1, 1):
+                    print("Won on a diagonal from top left to bottom right")
+                    return True
 
-        # End diagonal check
+        # Check all diagonals from top right to bottom left
+        for start_row in range(self.board.num_cells - self.in_row_to_win + 1):
+            for start_col in range(self.in_row_to_win - 1, self.board.num_cells):
+                if check_diagonal(start_row, start_col, 1, -1):
+                    print("Won on a diagonal from top right to bottom left")
+                    return True
 
 
         return False
